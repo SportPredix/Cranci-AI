2e55b41bfc00124b5a22685478ce2d6b69edff20
cranci1 <100066266+cranci1@users.noreply.github.com>
2026-04-04 20:18:43 +0200

    Initial Commit

---
+++ bocco/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  bocco
+//
+//  Created by Francesco on 04/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ bocco/boccoApp.swift
@@ -0,0 +1,17 @@
+//
+//  boccoApp.swift
+//  bocco
+//
+//  Created by Francesco on 04/04/26.
+//
+
+import SwiftUI
+
+@main
+struct boccoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
